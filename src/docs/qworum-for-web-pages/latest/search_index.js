9718f4cac51f6f1609448634aaee4abc694a26b7
Doğa Armangil <doga.armangil@alumni.epfl.ch>
2024-03-18 18:04:14 +0100

    Update the JS library docs

---
+++ src/docs/qworum-for-web-pages/latest/search_index.js
@@ -0,0 +1,3 @@
+(function () {
+  window.DENO_DOC_SEARCH_INDEX = {"nodes":[{"kind":["class"],"name":"Qworum","file":".","location":{"filename":"","line":2461,"col":0,"byteIndex":97398},"declarationKind":"export","deprecated":false}]};
+})()
