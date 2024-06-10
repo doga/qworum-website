0da863d28f917ada271f3531440b9cd316671300
Doğa Armangil <doga.armangil@alumni.epfl.ch>
2024-06-10 22:48:42 +0200

    add wavebox to supported browsers

--- build/assets/js/homepage.js
+++ build/assets/js/homepage.js
@@ -1,94 +1,41 @@
 import {
   detectBrowser,
-  // Browser,
-  ChromiumDesktop, EdgeDesktop, OperaDesktop, FirefoxDesktop, SafariDesktop,
-  // FirefoxAndroid, 
-  // KiwiAndroid,
-  // SafariMobile
-} from 'https://esm.sh/gh/doga/browser-detector@0.2.2/mod.mjs';
+  Browser, 
+  ChromeDesktop, EdgeDesktop, OperaDesktop, FirefoxDesktop, SafariDesktop,
+  VivaldiDesktop, WaveboxDesktop, ArcDesktop, BraveDesktop,
+  FirefoxAndroid, KiwiAndroid,
+  SafariMobile
+} from 'https://esm.sh/gh/doga/browser-detector@0.3.0/mod.mjs';
 
 highlightUsersBrowserForDownload();
 
 function highlightUsersBrowserForDownload() {
   const
-  ua         = window.navigator.userAgent,
-  browser    = detectBrowser(ua),
-  browserIds = [],
-  browserClasses   = {
-    edge      : EdgeDesktop,
-    opera     : OperaDesktop,
-    firefox   : FirefoxDesktop,
-    safari    : SafariDesktop,
-    // "safari-m": SafariMobile,
-    // "kiwi-a"  : KiwiAndroid,
-  };
+  ua             = window.navigator.userAgent,
+  browsers       = detectBrowser(ua),
+  cssClassToBrowserClass = new Map([
+    ['chrome' , ChromeDesktop],
+    ['safari' , SafariDesktop],
+    ['edge'   , EdgeDesktop],
+    ['wavebox', WaveboxDesktop],
+    ['opera'  , OperaDesktop],
+    ['vivaldi', VivaldiDesktop],
+    ['arc'    , ArcDesktop],
+    ['safarim', SafariMobile],
+    ['kiwi'   , KiwiAndroid],
+  ]);
 
-  // Find the browsers to highlight
-  if (browser instanceof ChromiumDesktop) {
-    if (navigator.brave) {
-      browserIds.push('brave');
-    } else {
-      ['chrome', 'vivaldi', 'arc'].forEach(brand => browserIds.push(brand));
-    }
+  if(
+    browsers.find(browser => browser instanceof BraveDesktop) &&
+    navigator.brave
+  ){
+    document.querySelector(`.download#brave`).classList.add('selected');
+    return;
   }
 
-  for (const browserId in browserClasses) {
-    if (Object.hasOwnProperty.call(browserClasses, browserId)) {
-      const browserClass = browserClasses[browserId];
-      // console.debug(`browserId: ${browserId}`);
-      if(browser instanceof browserClass){
-        browserIds.push(browserId)
-      }
-    }
-  }
-
-  // Highlight the browsers on the web page
-  for (const browserId of browserIds) {
-    document.querySelector(`.download#${browserId}`).classList.add('selected');
+  for (const [id, browserClass] of cssClassToBrowserClass) {
+    if(!browsers.find(browser => browser instanceof browserClass))continue;
+    document.querySelector(`.download#${id}`).classList.add('selected');
   }
 
 }
-
-// browser detection https://www.skypack.dev/view/bowser
-// import bowser from 'https://cdn.skypack.dev/pin/bowser@v2.11.0-Py0WXWOOzz0xiCAlkqky/mode=imports,min/optimized/bowser.js';
-// if(bowser)highlightUsersBrowserForDownload(); // BUG Browsers use webkit on ios!!! Chrome does not have extensions on mobile (https://support.google.com/chrome_webstore/answer/2664769?hl=en#zippy=%2Cinstall-on-your-phone), but chrome for mobile can install a chrome web store extension to chrome desktop.
-
-// function highlightUsersBrowserForDownload() {
-//   const 
-//   browser     = bowser.getParser(window.navigator.userAgent),
-//   browserName = browser.getBrowserName();
-
-//   // console.log(`user-agent: "${window.navigator.userAgent}"`);
-//   // console.log(`browser name: "${browserName}"`);
-//   // The current browser name is "Internet Explorer"
-
-//   switch (browserName) {
-//     case 'Chrome':
-//       if (navigator.brave) {
-//         document.querySelector('.download#brave').classList.add('selected');
-//       } else {
-//         document.querySelector('.download#chrome').classList.add('selected');
-//       }
-//       break;
-
-//     case 'Safari':
-//       document.querySelector('.download#safari').classList.add('selected');
-//       break;
-
-//     case 'Microsoft Edge':
-//       document.querySelector('.download#edge').classList.add('selected');
-//       break;
-
-//     case 'Firefox':
-//       document.querySelector('.download#firefox').classList.add('selected');
-//       break;
-
-//     case 'Opera':
-//       document.querySelector('.download#opera').classList.add('selected');
-//       break;
-
-//     default:
-//       break;
-//   }
-
-// }
